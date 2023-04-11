c38aa1783f98ee52727df1a8581c265af7b95a9a
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2023-04-11 23:51:37 +0200

    [nvim] Add treesitter-based folding

--- nvim/.config/nvim/lua/barthap/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/barthap/plugins/treesitter.lua
@@ -72,6 +72,12 @@ return {
 		---@param opts TSConfig
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
+
+      -- set up treesitter-based folding
+      local opt = vim.opt
+      opt.foldlevel = 20
+      opt.foldmethod = "expr"
+      opt.foldexpr = "nvim_treesitter#foldexpr()"
 		end,
 	},
 }
